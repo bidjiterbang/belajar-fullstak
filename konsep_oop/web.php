4319c8d6c27a8307603e96ca8f686528e004fd3b
bidjiterbang <smkiu.008@gmail.com>
2025-07-24 13:52:50 +0700

    first commit

---
+++ konsep_oop/web.php
@@ -0,0 +1,16 @@
+<?php
+
+class Product
+{
+    public $name;
+    public $price;
+    public $description;
+
+    public function __construct($name,$price,$description)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->description = $description;
+    }
+}
+$prduct1 = new product('smartphone','5000000','smartphone terbaru dengan kamera berkualitas tinggi');
